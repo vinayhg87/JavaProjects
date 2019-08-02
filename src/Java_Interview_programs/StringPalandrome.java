2c68f2eb15097dad4d338836f27d94a7906c9a97
Vinay <vinayhg87@gmail.com>
2019-08-02 16:32:46 +0530

    no Change

--- src/Java_Interview_programs/StringPalandrome.java
+++ src/Java_Interview_programs/StringPalandrome.java
@@ -7,27 +7,17 @@ public class StringPalandrome {
 	public static void main(String[] args) {
 		
 		System.out.println("Enter the string");
-		Scanner scr= new Scanner(System.in);
-		String pal = scr.nextLine();
+		Scanner scr =  new Scanner(System.in);
+		String Str=scr.nextLine();
 		String reverse="";
-		int len = pal.length();
 		
-		for(int i=0;i<len;i++)
+		for(int i=0;i<Str.length();i++)
 		{
-			reverse=pal.charAt(i)+reverse;
+			reverse=Str.charAt(i)+reverse;
 		}
+		
 		System.out.println(reverse);
-		
-		if(reverse.equals(pal))
-		{
-			System.out.println("String is Palandrome");
-		}
-		else
-		{
-			System.out.println("String is not Palandrome");
-		}
-		
+			
 		scr.close();
 	}
-
 }
